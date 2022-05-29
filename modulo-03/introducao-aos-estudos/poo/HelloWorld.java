aadf076a97ddde11f58d8f051c4a004885c466d1
Diogo Izele <diogogabrielizele@gmail.com>
2022-05-28 21:09:18 -0300

    feat: add some files about bluej test samples

---
+++ modulo-03/introducao-aos-estudos/poo/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Estou sendo impresso na tela");
+    System.out.println("Não estou bugando");
+  }
+}
